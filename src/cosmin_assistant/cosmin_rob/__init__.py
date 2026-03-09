59b8e1a5ea94e0eda235d8e2028d9ab8f4f8cc17
Acbeek <ancobeekman@me.com>
2026-03-09 20:06:26 +0100

    Modified cosmin_rob for addition check boxes

--- src/cosmin_assistant/cosmin_rob/__init__.py
+++ src/cosmin_assistant/cosmin_rob/__init__.py
@@ -5,6 +5,18 @@ from cosmin_assistant.cosmin_rob.aggregation import (
     WORST_SCORE_COUNTS_RULE,
     aggregate_box_assessment,
 )
+from cosmin_assistant.cosmin_rob.box1_prom_development import (
+    BOX_1_ITEM_CODES,
+    BOX_1_KEY,
+    BOX_1_MEASUREMENT_PROPERTY,
+    assess_box1_prom_development,
+)
+from cosmin_assistant.cosmin_rob.box2_content_validity import (
+    BOX_2_ITEM_CODES,
+    BOX_2_KEY,
+    BOX_2_MEASUREMENT_PROPERTY,
+    assess_box2_content_validity,
+)
 from cosmin_assistant.cosmin_rob.box3_structural_validity import (
     BOX_3_ITEM_CODES,
     BOX_3_KEY,
@@ -17,11 +29,41 @@ from cosmin_assistant.cosmin_rob.box4_internal_consistency import (
     BOX_4_MEASUREMENT_PROPERTY,
     assess_box4_internal_consistency,
 )
+from cosmin_assistant.cosmin_rob.box5_cross_cultural_validity import (
+    BOX_5_ITEM_CODES,
+    BOX_5_KEY,
+    BOX_5_MEASUREMENT_PROPERTY,
+    assess_box5_cross_cultural_validity_measurement_invariance,
+)
 from cosmin_assistant.cosmin_rob.box6_reliability import (
     BOX_6_ITEM_CODES,
     BOX_6_KEY,
     BOX_6_MEASUREMENT_PROPERTY,
     assess_box6_reliability,
+)
+from cosmin_assistant.cosmin_rob.box7_measurement_error import (
+    BOX_7_ITEM_CODES,
+    BOX_7_KEY,
+    BOX_7_MEASUREMENT_PROPERTY,
+    assess_box7_measurement_error,
+)
+from cosmin_assistant.cosmin_rob.box8_criterion_validity import (
+    BOX_8_ITEM_CODES,
+    BOX_8_KEY,
+    BOX_8_MEASUREMENT_PROPERTY,
+    assess_box8_criterion_validity,
+)
+from cosmin_assistant.cosmin_rob.box9_hypotheses_testing import (
+    BOX_9_ITEM_CODES,
+    BOX_9_KEY,
+    BOX_9_MEASUREMENT_PROPERTY,
+    assess_box9_hypotheses_testing_for_construct_validity,
+)
+from cosmin_assistant.cosmin_rob.box10_responsiveness import (
+    BOX_10_ITEM_CODES,
+    BOX_10_KEY,
+    BOX_10_MEASUREMENT_PROPERTY,
+    assess_box10_responsiveness,
 )
 from cosmin_assistant.cosmin_rob.item_utils import (
     build_item_assessment,
@@ -30,23 +72,51 @@ from cosmin_assistant.cosmin_rob.item_utils import (
 from cosmin_assistant.cosmin_rob.models import BoxAssessmentBundle, BoxItemInput
 
 __all__ = [
+    "BOX_10_ITEM_CODES",
+    "BOX_10_KEY",
+    "BOX_10_MEASUREMENT_PROPERTY",
+    "BOX_1_ITEM_CODES",
+    "BOX_1_KEY",
+    "BOX_1_MEASUREMENT_PROPERTY",
+    "BOX_2_ITEM_CODES",
+    "BOX_2_KEY",
+    "BOX_2_MEASUREMENT_PROPERTY",
     "BOX_3_ITEM_CODES",
     "BOX_3_KEY",
     "BOX_3_MEASUREMENT_PROPERTY",
     "BOX_4_ITEM_CODES",
     "BOX_4_KEY",
     "BOX_4_MEASUREMENT_PROPERTY",
+    "BOX_5_ITEM_CODES",
+    "BOX_5_KEY",
+    "BOX_5_MEASUREMENT_PROPERTY",
     "BOX_6_ITEM_CODES",
     "BOX_6_KEY",
     "BOX_6_MEASUREMENT_PROPERTY",
+    "BOX_7_ITEM_CODES",
+    "BOX_7_KEY",
+    "BOX_7_MEASUREMENT_PROPERTY",
+    "BOX_8_ITEM_CODES",
+    "BOX_8_KEY",
+    "BOX_8_MEASUREMENT_PROPERTY",
+    "BOX_9_ITEM_CODES",
+    "BOX_9_KEY",
+    "BOX_9_MEASUREMENT_PROPERTY",
     "BoxAssessmentBundle",
     "BoxItemInput",
     "NA_HANDLING_RULE",
     "WORST_SCORE_COUNTS_RULE",
     "aggregate_box_assessment",
+    "assess_box10_responsiveness",
+    "assess_box1_prom_development",
+    "assess_box2_content_validity",
     "assess_box3_structural_validity",
     "assess_box4_internal_consistency",
+    "assess_box5_cross_cultural_validity_measurement_invariance",
     "assess_box6_reliability",
+    "assess_box7_measurement_error",
+    "assess_box8_criterion_validity",
+    "assess_box9_hypotheses_testing_for_construct_validity",
     "build_item_assessment",
     "build_item_assessments_for_box",
 ]
